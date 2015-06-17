a1da8e8a4710a05afa1100cfdf731444e979af36
ethanabowen <ethan.a.bowen@gmail.com>
2015-06-17 00:40:07 -0500

    file cleanup
    file cleanup

--- app.js
+++ app.js
@@ -19,30 +19,16 @@ app.get('/', function (req, res) {
 
 app.get('/simpleGet', function (req, res) {
     console.log('Server - Simple Get Triggered');
-    //var json = JSON.parse(req.query);
-    console.log(req.query);
-    //console.log(json.ticker);
     // input value from search
     var val = req.query.ticker.trim();//console.log(val);
     console.log(val);
-// url used to search yql
-
+    // url used to search yql
     var url = "http://finance.yahoo.com/q/h?s=" + val;
     console.log(url);
 
 // request module is used to process the yql url and return the results in JSON format
     request(url, function(err, resp, body) {
-        console.log(body);
-
-        /*// logic used to compare search results with the input from user
-        // console.log(!body.query.results.RDF.item['about'])
-        if (!body.query.results.RDF.item['about'] === false) {
-            craig = "No results found. Try again.";
-        } else {
-            results = body.query.results.RDF.item[0]['about']
-            craig = '<a href ="'+results+'">'+results+'</a>'
-        }
-        // pass back the results to client side*/
+        //console.log(body);
         res.send(body);
     });
 
